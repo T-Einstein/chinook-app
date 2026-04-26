2be6a6f90749ff03da77714f9da7b68957f481e7
Tsohle <tsohlemasia06@gmail.com>
2026-04-26 13:36:59 +0200

    added Employee GUI logic

--- src/main/java/com/Tsohle/chinook/EmployeeGUI.java
+++ src/main/java/com/Tsohle/chinook/EmployeeGUI.java
@@ -1,4 +1,41 @@
 package com.Tsohle.chinook;
-public class EmployeeGUI {
-    
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class EmployeeGUI extends JFrame {
+    JTable table;
+    DefaultTableModel model; 
+
+    public EmployeeGUI(){
+        setTitle("Emloyees");
+        setSize(800,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        String[] columns = {
+                "First Name", "Last Name", "Title",
+                "City", "Country", "Phone", "Supervisor"
+        };
+
+        model = new DefaultTableModel(columns, 0);
+        table = new JTable(model);
+
+        add(new JScrollPane(table), BorderLayout.CENTER);
+
+        loadEmployees();
+
+        setVisible(true);
+    }
+
+     private void loadEmployees() {
+        model.setRowCount(0); 
+
+        ArrayList<Object[]> data = EmployeeDAO.getEmployees();
+
+        for (Object[] row : data) {
+            model.addRow(row);
+        }
+    }
 }
